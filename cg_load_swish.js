67c051ee68bdf59ee265d4caac7e58b11272fec6
Brett Simpson <brett@codegravity.se>
2021-02-05 10:12:30 +0100

    Add files via upload

---
+++ cg_load_swish.js
@@ -0,0 +1,13 @@
+
+var clientTel = jQuery('#billing_phone').val();
+var clientTelstr1 =  clientTel.substr(0,3)
+var clientTelstr2 = '*** ** **';
+
+//jQuery("#swish-payer-alias").val(jQuery('#billing_phone').val());	
+//jQuery("#swish-payer-alias").val(clientTelstr1.concat(clientTelstr2));	
+
+jQuery(".woocommerce-additional-fields h3").css("display": "none!important");
+// hide  order comments with  kund id
+//document.getElementById('order_comments').type = 'hidden';
+
+  
