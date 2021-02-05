12779dbe9ac6583172bb29c7ea05f8a648d1af8a
Brett Simpson <brett@codegravity.se>
2021-02-05 10:20:04 +0100

    Update cg_load_swish.js

--- cg_load_swish.js
+++ cg_load_swish.js
@@ -3,11 +3,12 @@ var clientTel = jQuery('#billing_phone').val();
 var clientTelstr1 =  clientTel.substr(0,3)
 var clientTelstr2 = '*** ** **';
 
+// option 1 copy client tel to Swish directly (potential security issue)
 //jQuery("#swish-payer-alias").val(jQuery('#billing_phone').val());	
+
+// option 2 copy part of client tel to Swish 
 //jQuery("#swish-payer-alias").val(clientTelstr1.concat(clientTelstr2));	
 
-jQuery(".woocommerce-additional-fields h3").css("display": "none!important");
-// hide  order comments with  kund id
-//document.getElementById('order_comments').type = 'hidden';
+
 
   
